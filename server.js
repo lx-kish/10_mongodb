f09931e0c8aac617b91892fcdeb8aae2270fe33e
lx kish <urban.ktulhu@gmail.com>
2018-08-12 19:20:42 +1200

    125 Mongo Driver: inserting multiple and getting data

--- server.js
+++ server.js
@@ -10,18 +10,45 @@ const url = 'mongodb://localhost:27017/test';
 //     db.close();
 // })
 
+// MongoClient.connect(url, (err, db) => {
+
+//     const cars = [
+//         {model:"Chevy",year:"2017"},
+//         {model:"Volkswagen",year:"2009"}
+//     ];
+
+//     db.collection('Cars').insertMany(cars, (err, res) => {
+//         if(err){
+//             return console.log(`Cannot insert: ${err}`)
+//         }
+//         console.log(res.ops)
+//     })
+
+//     db.close();
+// })
+
+// MongoClient.connect(url, (err, db) => {
+//     db.collection('Cars').find()
+//     .skip(1)
+//     .limit(1)
+//     .sort({"_id":-1})
+//     .toArray().then(data => {
+//         console.log(data)
+//     });
+//     db.close();
+// })
+
+// MongoClient.connect(url, (err, db) => {
+//     db.collection('Cars').find({year:'2009'})
+//     .toArray().then(data => {
+//         console.log(data)
+//     });
+//     db.close();
+// })
+
 MongoClient.connect(url, (err, db) => {
-
-    db.collection('Cars').insertOne({
-        _id:23,
-        model:'Toyota',
-        year:2009
-    }, (err, res) => {
-        if(err){
-            return console.log(`Cannot insert: ${err}`)
-        }
-        console.log(res.ops)
-    })
-
+    db.collection('Cars').findOne({year:'2009'}, {model:0, year:0}, (err, doc) => {
+        console.log(doc)
+    });
     db.close();
 })
