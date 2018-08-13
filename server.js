67e960d286889e71d74d315cc5bb4473c4146245
lx kish <urban.ktulhu@gmail.com>
2018-08-14 09:29:29 +1200

    129 Using Postman (we will use it next)

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/test');
+app.use(bodyParser.json());
 
 const carSchema = mongoose.Schema({
     brand: String,
@@ -59,14 +63,33 @@ const Car = mongoose.model('Car', carSchema);
 //     }
 // )
 
-Car.findById("5b701b9151e3ca18d453d9a8", (err, car) => {
-    if (err) return console.log(err)
-    car.set({
-        brand: "Porche",
-        model: "Cayene"
-    });
-    car.save((err, doc) => {
-        if (err) return console.log(err)
-        console.log(doc)    
-    })
+// Car.findById("5b701b9151e3ca18d453d9a8", (err, car) => {
+//     if (err) return console.log(err)
+//     car.set({
+//         brand: "Porche",
+//         model: "Cayene"
+//     });
+//     car.save((err, doc) => {
+//         if (err) return console.log(err)
+//         console.log(doc)    
+//     })
+// })
+
+app.get('/api/movies', (req, res) => {
+    const movie = {
+        name: "Fight club",
+        gen: "Weird",
+        stars: ["Brad Pitt","Edward Norton"]
+    }
+    res.send(movie)
+})
+
+app.post('/api/movies', (req, res) => {
+    console.log(req.body)
+    res.sendStatus(200)
+})
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Started at port ${port}`)
 })
